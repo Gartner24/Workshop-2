e3614842ec7ecf6bfae5c4e58177f0d9939b973f
Gabriel Turizo <gabrielturizo13@gmail.com>
2022-06-09 16:29:16 -0500

    maquetado terminado

---
+++ src/containers/Profiles.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Container, Row, Col, InputGroup, Form, Button } from "react-bootstrap";
+import NavFooter from "../components/NavFooter";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FaRegComments } from "react-icons/fa";
+import { TbBrandTelegram } from "react-icons/tb";
+
+const Profiles = () => {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col>
+            <img
+              src="https://res.cloudinary.com/dy8n5pg1g/image/upload/v1654806139/workshop/final_l2epbr.png"
+              className="img-fluid rounded mx-auto d-block"
+            />
+            <Container fluid className="d-flex align-items-center mx-5 p-2">
+              <img
+                src="https://res.cloudinary.com/dy8n5pg1g/image/upload/v1654803731/workshop/p-4_pnsbhr.png"
+                className="img-fluid rounded-circle w-25"
+              />
+              <p className="fw-semibold mx-3">Jennie Kim</p>
+              <AiOutlineHeart />
+              <FaRegComments />
+              <TbBrandTelegram />
+            </Container>
+            <p className="lh-1 text-center my-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Imperdiet
+              gravida tortor in habitant pellentesque a quisque. Nisl diam, amet
+              eu est libero dignissim donec nec. Fames bibendum porta phasellus
+              neque. Integer et lectus amet, vitae facilisis laoreet feugiat
+              pellentesque accumsan. Turpis eget laoreet turpis urna tincidunt
+              nisl, integer nisl. Id nec tortor vel, dui, lectus. Donec
+              consequat dolor cursus sed pellentesque etiam ipsum, id quam.
+              Tincidunt eu duis ullamcorper posuere augue. Arcu senectus elit,
+              semper diam porta. Platea tempus augue ante pellentesque dictum
+              sed vitae. Auctor dui ac bibendum lacus.
+            </p>
+            <Container fluid className="d-flex align-items-center my-5 p-2">
+              <img
+                src="https://res.cloudinary.com/dy8n5pg1g/image/upload/v1654809352/workshop/profile_a0elvs.png"
+                className="img-fluid rounded-circle w-25 "
+              />
+              <InputGroup className="mb-3 ">
+                <Form.Control aria-label="First name" />
+                <Button><TbBrandTelegram /></Button>
+              </InputGroup>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+
+      <NavFooter />
+    </>
+  );
+};
+
+export default Profiles;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../containers/Home'
+import Profiles from '../containers/Profiles'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/Profiles" element={<Profiles />} />
         </Routes>
     </BrowserRouter>
   )
